6c40b91988b722ba7f78ce4a93dd842128af0fc5
TienYou-Nathan <nathan.tienyou@viacesi.fr>
2018-11-19 08:49:50 +0100

    added folder

---
+++ Partie 3/Debug/params.h
@@ -0,0 +1,12 @@
+    /*
+    * params.h
+    *
+    *      Author: legon
+    */
+    #ifndef PARAMS_H_
+    #define PARAMS_H_
+
+    #define LIGHT 5
+    #define LED 7
+
+    #endif /* PARAMS_H_ */
